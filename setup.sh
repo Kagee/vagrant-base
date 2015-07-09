c7b863a0343335f2359384a3b10feb0cb705db34
Anders Einar Hilden <hildenae@gmail.com>
2015-07-09 18:05:20 +0200

    Better VM detection and apt-proxy handling

--- setup.sh
+++ setup.sh
@@ -2,13 +2,27 @@
 set -e
 set -x
 
-VMXNET=$(cat /proc/modules | grep -q vmxnet; echo $?)
-if [ $VMXNET -eq 0 ]; then
-  echo "On virtual machine, setting up apt proxy"
+IS_VMWARE=$(cat /proc/modules | grep -c 'vmxnet')
+IS_VBOX=$(cat /proc/modules | grep -c 'vboxsf')
+if [ $IS_VMWARE -gt 0 ] || [ $IS_VBOX -gt 0 ]; then
+  echo "On virtual machine, testing if apt proxy is avalible"
+  # For a default NAT setup on Virtualbox, the host is the default gateway
   GATEWAY=$(ip route show | grep ^default | cut -d' ' -f 3)
-  HOST="$(echo -n $GATEWAY | cut -d . -f 1-3).$(($(echo $GATEWAY | cut -d . -f 4 )-1))"
-  echo "Acquire::http { Proxy \"http://$HOST:3142\"; };" >> /etc/apt/apt.conf.d/01proxy
+  HOST=$GATEWAY
+  if [ $IS_VMWARE -gt 0 ]; then
+    # For a default NAT setup on Vmware, the host is the default gateway minus one
+    HOST="$(echo -n $GATEWAY | cut -d . -f 1-3).$(($(echo $GATEWAY | cut -d . -f 4 )-1))"
+  fi
+  APT_PROXY="http://$HOST:3142"
+  HAS_PROXY=$(wget $APT_PROXY 2>&1 | grep -c 'ERROR 404: Usage Information';)
+  if [ $HAS_PROXY -eq 1 ]; then
+    echo "There appears to be a apt proxy on $APT_PROXY, using it"
+    echo "Acquire::http { Proxy \"$APT_PROXY\"; };" >> /etc/apt/apt.conf.d/01proxy
+  else
+    echo "Test for apt proxy on $APT_PROXY failed, not using proxy" 1>&2
+  fi
 fi
+
 
 # Some standard tools
 apt-get install --yes language-pack-nb vim htop most git
